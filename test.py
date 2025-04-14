7ceba915a4d8bf198aab6aa011f86c176adeda92
luthfi <luthlirik@gmail.com>
2025-04-15 04:37:51 +0700

    Async Support

--- test.py
+++ test.py
@@ -1,15 +1,31 @@
 import komiku
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from typing import TypedDict, AsyncIterator
+from starlette.applications import Starlette
+from starlette.requests import Request
+import contextlib
+import httpx
 
-app = FastAPI()
+class State(TypedDict):
+    http_client: httpx.AsyncClient
+
+@contextlib.asynccontextmanager
+async def lifespan(app: Starlette) -> AsyncIterator[State]:
+    async with httpx.AsyncClient() as client:
+        yield {"http_client": client}
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/search")
-def read_root(q:str):
-    return komiku.Search(q).result
+async def read_root(q:str, request: Request):
+    asd = await komiku.Search(q).asyn(request.state.http_client)
+    return asd.result
 
 @app.get("/manga/{slug}")
-def read_root(slug:str):
-    return komiku.Manga(slug).result
+async def read_root(slug:str, request: Request):
+    asd = await komiku.Manga(slug).asyn(request.state.http_client)
+    return asd.result
 
 @app.get("/chapter/{slug}")
 def get_chapter(slug:str):
